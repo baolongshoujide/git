9a0d1480ca70d9d0e2996693472e7fbef99389e4
baolongshou <yzz3328679910@qq.com>
2026-08-22 10:36:19 +0800

    pinia内数据解构丢失响应式及解决办法

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,16 @@
   import Son1 from './components/Son1.vue';
   import Son2 from './components/Son2.vue';
   import {Store} from '@/store'
+  import {storeToRefs} from 'pinia'
   const useone = Store()
   console.log(useone);
-  
+  // 解构后丢失响应式
+  const {count,double} = storeToRefs(useone)
 </script>
 
 <template>
     <div class="box">
-      <h3>App.vue根组件 - {{ useone.count }}-{{ useone.double }}</h3>
+      <h3>App.vue根组件 - {{ count }}-{{ double }}</h3>
       <Son1></Son1>
       <Son2></Son2>
     </div>
